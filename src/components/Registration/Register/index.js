dcf2435a5cc169fc8383a5b2b8d3b97882c81457
aaris ahmed <aaris.ahmed1999@gmail.com>
2024-11-14 20:09:10 +0530

    login form

--- src/components/Registration/Register/index.js
+++ src/components/Registration/Register/index.js
@@ -1,19 +1,33 @@
 import { useState } from "react";
 
-export default function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
+export default function Register({ setIsAuthenticated }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (email && password) {
+      localStorage.setItem("userEmail", email);
+      localStorage.setItem("userPassword", password);
+
+      setIsAuthenticated(true);
+    } else {
+      setError("Please fill both fields");
+    }
+  };
 
   return (
     <div>
       <p>Registration form</p>
-      <input
+      <form onSubmit={handleRegister}>
+        <input
           type="email"
           value={email}
           placeholder="email"
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
 
         <input
@@ -21,7 +35,12 @@ export default function Register() {
           value={password}
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
+
+        <button type="submit">Register</button>
+      </form>
+      {error && <p>{error}</p>}
     </div>
   );
 }
--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -3,12 +3,13 @@ import Login from './Login';
 import Register from './Register';
 
 export default function Registration() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     return (
         <div>
-            <h1>Task 1</h1>
+            <h1>Task 1, will be using localStorage for it</h1>
             <div>
                 {/* <form>
                     <input 
@@ -25,8 +26,8 @@ export default function Registration() {
                         onChange={e => setPassword(e.target.value)}
                     /> */}
 
-                    <Login />
-                    <Register />
+                    <Login setIsAuthenticated={setIsAuthenticated}/>
+                    <Register setIsAuthenticated={setIsAuthenticated}/>
                     {/* <div><button>Sign up</button></div>
                 </form> */}
             </div>
